ad550525ec04e8284bbd9de3bd932b55f3d0d9b9
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2019-12-22 09:45:59 -0800

    Add Flask app

--- worklog.py
+++ worklog.py
@@ -1,11 +1,10 @@
+from flask import Flask, escape, request, render_template
+
 import sqlite3
 
 def _quote(param):
     quoted = param.replace('"', '""')
     return f'"{quoted}"'
-
-
-
 
 def search(cursor, user_input):
     safe_input = _quote(user_input)
@@ -16,12 +15,33 @@ def search(cursor, user_input):
     cursor.execute(sql, (safe_input,))
     return cursor.fetchall()
 
+def getall(cursor):
+    sql = """
+      select * from worklogs
+      order by workdate desc
+    """
+    cursor.execute(sql)
+    return cursor.fetchall()
 
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('test.db', check_same_thread=False)
 conn.row_factory = sqlite3.Row
 cur = conn.cursor()
-user_input = input('search> ')
-for result in search(cur, user_input):
-    print(result)
-    for key in sorted(result.keys()):
-        print(f"{key}={result[key]}")
+
+
+
+# user_input = input('search> ')
+# for result in search(cur, user_input):
+#     print(result)
+#     for key in sorted(result.keys()):
+#         print(f"{key}={result[key]}")
+
+
+app = Flask(
+    __name__,
+    template_folder="app/templates",
+)
+
+@app.route('/')
+def hello():
+    all = getall(cur)
+    return render_template('index.html', worklogs=all)
